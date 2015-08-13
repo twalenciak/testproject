e3b5bfc17b737b6fbb54b6c248d72869964883bb
Bitcats_in <bitcat.in@gmail.com>
2015-08-13 17:59:20 +0200

    added single view project

---
+++ TestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestProject
+//
+//  Created by Bitcats on 13/08/15.
+//  Copyright (c) 2015 TWalenciak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
